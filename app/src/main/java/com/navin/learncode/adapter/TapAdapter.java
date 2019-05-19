b147fb08daa8216144b6ae6b08a6b7be372aeef6
AmirJabbari <43641240+AmirJabbari@users.noreply.github.com>
2019-05-19 13:03:10 +0430

    Initial commit

---
+++ app/src/main/java/com/navin/learncode/adapter/TapAdapter.java
@@ -0,0 +1,39 @@
+package com.navin.learncode.adapter;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TapAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> mFragmentList=new ArrayList<>();
+    private List<String> mFragmentTitleList=new ArrayList<>();
+
+    public TapAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
